6763de9a72e82f2dbbacf0b5fbb90301955c848d
Beatriz Muñoz <15beatriznoemi@gmail.com>
2023-01-28 22:38:23 -0300

    hice condicional sass, media queries en todas las paginas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const ciudad = document.querySelector("#nav");
+
+const ciudad= document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
   ciudad.classList.add("visible");
-})
+  abrir.classList.add("invisiblemenu");
+});
 cerrar.addEventListener("click", () => {
-    ciudad.classList.remove("visible");
-})
+  ciudad.classList.remove("visible");
+  abrir.classList.remove("invisiblemenu");
+});
